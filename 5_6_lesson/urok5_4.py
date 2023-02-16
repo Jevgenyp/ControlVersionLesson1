8d0aeef9bd797bd4bd7325f020febbc4a6cf78bb
Jevgenijs Pitkevics <Jpcoatingss@gmail.com>
2023-02-16 20:56:58 +0100

    task4

---
+++ 5_6_lesson/urok5_4.py
@@ -0,0 +1,12 @@
+with open('3.txt','r') as file:
+    a = file.read()
+count = 1
+res = ''
+for i in range(0,len(a)-1):
+    if a[i]==a[i+1]:
+        count+=1
+    else:
+        res = res +str(count)+a[i]
+        count=1
+res = res +str(count)+a[i]
+print(res)
