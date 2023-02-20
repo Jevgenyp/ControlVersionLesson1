9384134fc86d1b32da3dbe2c1639e4a85bc761a6
Jevgenijs Pitkevics <Jpcoatingss@gmail.com>
2023-02-20 22:44:39 +0100

    next level

--- 7_8_lesson/1.py
+++ 7_8_lesson/1.py
@@ -1,9 +1,13 @@
-b = 5
-c = 0
-try:
-    a = b / c
-except ZeroDivisionError: 
-    print('На ноль делить нельзя')
-    a = 0
-print(a)
+# b = 5
+# c = 0
+# try:
+#     a = b / c
+# except ZeroDivisionError: 
+#     print('На ноль делить нельзя')
+#     a = 0
+# print(a)
     
+file = open('C:/Users/Jev/Desktop/GeekBrains/Python/HomeworkPy/Homework_Phyton/7_8_lesson/1.txt', 'r', encoding='utf-8')
+text = file.read()
+print(text)
+file.close()
